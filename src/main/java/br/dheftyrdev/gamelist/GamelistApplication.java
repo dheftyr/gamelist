92bced3dfaaa8f0f0ebaf28e82838405e85b1cb6
DheftyrDev <dheftyr_fullstack@hotmail.com>
2024-11-15 18:05:19 -0300

    Criado o projeto Spring Boot API GameList e atualindo o lib Maven

---
+++ src/main/java/br/dheftyrdev/gamelist/GamelistApplication.java
@@ -0,0 +1,13 @@
+package br.dheftyrdev.gamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelistApplication.class, args);
+	}
+
+}
